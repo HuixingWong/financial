139216b66a2f3f90d3d94c8f696e8a3f0e0fe499
unDeathDream <whx24@live.com>
2020-11-20 10:47:39 +0800

    read token from local

--- app/src/main/java/com/huixing/financial/network/HttpRequestInterceptor.kt
+++ app/src/main/java/com/huixing/financial/network/HttpRequestInterceptor.kt
@@ -1,5 +1,6 @@
 package com.huixing.financial.network
 
+import com.huixing.financial.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 import timber.log.Timber
@@ -7,7 +8,8 @@ import timber.log.Timber
 class HttpRequestInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
-        val request = originalRequest.newBuilder().url(originalRequest.url).build()
+        val request = originalRequest.newBuilder().url(originalRequest.url)
+            .header("token",BuildConfig.ACCESS_TOKEN).build()
         Timber.d(request.toString())
         return chain.proceed(request)
     }
