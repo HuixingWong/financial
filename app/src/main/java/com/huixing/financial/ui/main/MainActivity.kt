53fe9c0b27ad67bc4345da76e1c1c9b7c4b0e4b6
unDeathDream <whx24@live.com>
2020-11-25 10:10:14 +0800

    use drawer navigate with navigation

--- app/src/main/java/com/huixing/financial/ui/main/MainActivity.kt
+++ app/src/main/java/com/huixing/financial/ui/main/MainActivity.kt
@@ -1,20 +1,65 @@
 package com.huixing.financial.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 import com.huixing.financial.R
 import com.huixing.financial.base.DataBindingActivity
+import com.huixing.financial.base.viewmodel.SharedViewModel
+import com.huixing.financial.ui.search.SearchFundActivity
+import com.huixing.financial.utils.appViewModels
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
-class MainActivity : DataBindingActivity() {
+class MainActivity : DataBindingActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val sharedViewModel: SharedViewModel by appViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBar()
     }
+
+
+    private fun setupActionBar() {
+        setSupportActionBar(mToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        menu.findItem(R.id.search).setOnMenuItemClickListener {
+            Intent(this, SearchFundActivity::class.java).apply {
+                startActivity(this)
+            }
+            return@setOnMenuItemClickListener false
+        }
+        sharedViewModel.showSearch.observe(this, {
+            menu.findItem(R.id.search).isVisible = it
+        })
+        return true
+    }
+
 
     override fun onBackPressed() {
         moveTaskToBack(true)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return true
     }
 
 }
--- app/src/main/java/com/huixing/financial/ui/main/MainFragment.kt
+++ app/src/main/java/com/huixing/financial/ui/main/MainFragment.kt
@@ -1,46 +1,22 @@
 package com.huixing.financial.ui.main
 
-import android.content.Intent
-import android.view.Menu
-import android.view.MenuInflater
 import androidx.fragment.app.viewModels
 import com.huixing.financial.R
 import com.huixing.financial.base.DataBindingFragment
-import com.huixing.financial.base.viewmodel.SharedViewModel
 import com.huixing.financial.databinding.FragmentMainBinding
 import com.huixing.financial.ui.adapter.HotFundAdapter
-import com.huixing.financial.ui.search.SearchFundActivity
-import com.huixing.financial.utils.appViewModels
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainFragment: DataBindingFragment<FragmentMainBinding>(R.layout.fragment_main) {
 
     private val viewModel: MainViewModel by viewModels()
-    private val sharedViewModel: SharedViewModel by appViewModels()
-
-
-    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, menuInflater)
-        menuInflater.inflate(R.menu.options_menu, menu)
-        menu.findItem(R.id.search).setOnMenuItemClickListener {
-            Intent(requireActivity(), SearchFundActivity::class.java).apply {
-                startActivity(this)
-            }
-            return@setOnMenuItemClickListener false
-        }
-        sharedViewModel.showSearch.observe(this, {
-            menu.findItem(R.id.search).isVisible = it
-        })
-    }
 
     override fun bind() {
         binding?.apply {
-            setHasOptionsMenu(true)
             lifecycleOwner = this@MainFragment
             adapter = HotFundAdapter()
             vm = viewModel
-            svm = sharedViewModel
         }
     }
 
