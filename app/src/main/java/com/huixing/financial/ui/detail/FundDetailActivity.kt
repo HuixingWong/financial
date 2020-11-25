76e43f881bfd9a866a6cbb892bcf75ad90db48a8
unDeathDream <whx24@live.com>
2020-11-25 13:19:37 +0800

    revert

--- app/src/main/java/com/huixing/financial/ui/detail/FundDetailActivity.kt
+++ app/src/main/java/com/huixing/financial/ui/detail/FundDetailActivity.kt
@@ -4,32 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.huixing.financial.R
 import com.huixing.financial.base.DataBindingActivity
 import com.huixing.financial.databinding.ActivityFundDetailBinding
-import com.huixing.financial.repo.FundDetailRepo
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class FundDetailActivity : DataBindingActivity() {
 
-//    @Inject
-//    lateinit var fundViewModelFactory: FundDetailViewModel.AssistedFactory
-
     @Inject
-    lateinit var fundDetailRepo: FundDetailRepo
+    lateinit var fundViewModelFactory: FundDetailViewModel.AssistedFactory
 
     private val fundDetailViewModel: FundDetailViewModel by viewModels {
-        object : ViewModelProvider.Factory {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                return FundDetailViewModel(fundDetailRepo, intent.getStringExtra(FUND_CODE)?:"") as T
-            }
-        }
-
+        FundDetailViewModel.provideFactory(
+                fundViewModelFactory,
+                intent?.getStringExtra(FUND_CODE) ?: ""
+        )
     }
     private val binding by binding<ActivityFundDetailBinding>(R.layout.activity_fund_detail)
 
