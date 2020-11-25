907fef7d87068c0b9abebb9c45dca41c41b56152
unDeathDream <whx24@live.com>
2020-11-25 13:19:06 +0800

    not use assistedInject to inject viewModel, but in this way view will hold repo， so this is a wrong sample

--- app/src/main/java/com/huixing/financial/ui/detail/FundDetailActivity.kt
+++ app/src/main/java/com/huixing/financial/ui/detail/FundDetailActivity.kt
@@ -4,23 +4,32 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.huixing.financial.R
 import com.huixing.financial.base.DataBindingActivity
 import com.huixing.financial.databinding.ActivityFundDetailBinding
+import com.huixing.financial.repo.FundDetailRepo
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class FundDetailActivity : DataBindingActivity() {
 
+//    @Inject
+//    lateinit var fundViewModelFactory: FundDetailViewModel.AssistedFactory
+
     @Inject
-    lateinit var fundViewModelFactory: FundDetailViewModel.AssistedFactory
+    lateinit var fundDetailRepo: FundDetailRepo
 
     private val fundDetailViewModel: FundDetailViewModel by viewModels {
-        FundDetailViewModel.provideFactory(
-            fundViewModelFactory,
-            intent?.getStringExtra(FUND_CODE) ?: ""
-        )
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return FundDetailViewModel(fundDetailRepo, intent.getStringExtra(FUND_CODE)?:"") as T
+            }
+        }
+
     }
     private val binding by binding<ActivityFundDetailBinding>(R.layout.activity_fund_detail)
 
