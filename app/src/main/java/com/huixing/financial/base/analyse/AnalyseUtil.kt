34a35aa209529c8074a31fb93a81c6caccd124c4
HuixingWang <whx24@live.com>
2020-12-01 22:35:55 +0800

    add simple analyse

--- app/src/main/java/com/huixing/financial/base/analyse/AnalyseUtil.kt
+++ app/src/main/java/com/huixing/financial/base/analyse/AnalyseUtil.kt
@@ -2,6 +2,8 @@ package com.huixing.financial.base.analyse
 
 import androidx.annotation.WorkerThread
 import com.huixing.financial.model.FundDetail
+import kotlin.math.max
+import kotlin.math.min
 
 object AnalyseUtil {
 
@@ -11,9 +13,12 @@ object AnalyseUtil {
     ): FundDetail {
         val analysisResult = StringBuilder()
         var declineDays = 0
+        var maxProfit = 0.0
         kotlin.runCatching {
-            fundDetail.data?.netWorthData?.run {
-                asReversed().forEach {
+            val netWorthData = fundDetail.data?.netWorthData
+            val reverseData = netWorthData?.asReversed()
+            reverseData?.run {
+                forEach {
                     if (it[2].toDouble() < 0) {
                         declineDays++
                     } else {
@@ -21,10 +26,27 @@ object AnalyseUtil {
                     }
                 }
             }
+
+            netWorthData?.let {
+                maxProfit = analyseMaxProfit(it)
+            }
         }
-        analysisResult.append("连续下跌${declineDays}天")
+
+        analysisResult.append("连续下跌${declineDays}天\n")
+        analysisResult.append("\n最佳单次交易获利可达到${(maxProfit * 100).toInt()}%")
         fundDetail.analysisResult = analysisResult.toString()
         return fundDetail
+    }
+
+    @WorkerThread
+    fun analyseMaxProfit(worthData: List<List<String>>): Double {
+        var cost = Double.MAX_VALUE
+        var profit = 0.0
+        worthData.forEach {
+            cost = min(cost, it[1].toDouble())
+            profit = max(profit, it[1].toDouble() - cost)
+        }
+        return profit / cost
     }
 
 }
