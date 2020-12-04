3d0bb7d7cd1ccf332fbac810f5bf2e15aba4017d
HuixingWang <whx24@live.com>
2020-12-04 12:26:41 +0800

    formatting

--- app/src/main/java/com/huixing/financial/ui/rank/RankDialogFragment.kt
+++ app/src/main/java/com/huixing/financial/ui/rank/RankDialogFragment.kt
@@ -71,17 +71,13 @@ class RankDialogFragment : DataBindingDialogFragment<FragmentRankDialogBinding>(
         }
         val companyList = mutableListOf<String>()
         binding?.mCompanyGroup?.checkedChipIds?.forEach {
-            (view?.findViewById<Chip>(it)?.tag as? String)?.let { comp ->
-                companyList.add(comp)
-            }
+            (view?.findViewById<Chip>(it)?.tag as? String)?.let { comp -> companyList.add(comp) }
         }
         rankViewModel.rankParam.fundType = typeList
         rankViewModel.rankParam.fundCompany = companyList
         rankViewModel.rankParam.sort =
                 binding?.mSortGroup?.checkedChipId?.let {
-                    view?.findViewById<Chip>(
-                            it
-                    )?.tag
+                    view?.findViewById<Chip>(it)?.tag
                 } as? String
         rankViewModel.rankParam.asc = if (binding?.useDesc?.isChecked == true) 1 else 0
         rankViewModel.rankParam.pageSize = binding?.dataSize?.text.toString().toInt()
