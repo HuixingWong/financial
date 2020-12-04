27f771f14561ed0c157a96517c602c03cb2ac0be
HuixingWang <whx24@live.com>
2020-12-04 12:23:57 +0800

    remove synthetic ( google will abandoned this in 2021 )

--- app/src/main/java/com/huixing/financial/ui/rank/RankDialogFragment.kt
+++ app/src/main/java/com/huixing/financial/ui/rank/RankDialogFragment.kt
@@ -5,13 +5,12 @@ import androidx.fragment.app.activityViewModels
 import com.google.android.material.chip.Chip
 import com.huixing.financial.R
 import com.huixing.financial.base.DataBindingDialogFragment
-import com.huixing.financial.databinding.FragmentRankDialogBindingImpl
+import com.huixing.financial.databinding.FragmentRankDialogBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_rank_dialog.*
 
 @AndroidEntryPoint
-class RankDialogFragment : DataBindingDialogFragment<FragmentRankDialogBindingImpl>(
-    R.layout.fragment_rank_dialog
+class RankDialogFragment : DataBindingDialogFragment<FragmentRankDialogBinding>(
+        R.layout.fragment_rank_dialog
 ) {
 
     private val rankViewModel: RankViewModel by activityViewModels()
@@ -19,10 +18,6 @@ class RankDialogFragment : DataBindingDialogFragment<FragmentRankDialogBindingIm
     override fun bind() {
         binding?.apply {
             vm = rankViewModel
-            submit.setOnClickListener {
-                submitData()
-                dismiss()
-            }
             root.post {
                 bindView()
             }
@@ -30,57 +25,66 @@ class RankDialogFragment : DataBindingDialogFragment<FragmentRankDialogBindingIm
     }
 
     private fun bindView() {
-        mTypeGroup?.forEach {
+
+        binding?.submit?.setOnClickListener {
+            submitData()
+            dismiss()
+        }
+
+        binding?.mTypeGroup?.forEach {
             (it as? Chip)?.apply {
                 if (tag != null &&
-                    rankViewModel.rankParam.fundType?.contains(tag) == true
+                        rankViewModel.rankParam.fundType?.contains(tag) == true
                 ) {
                     isChecked = true
                 }
             }
         }
-        mCompanyGroup?.forEach {
+        binding?.mCompanyGroup?.forEach {
             (it as? Chip)?.apply {
                 if (tag != null &&
-                    rankViewModel.rankParam.fundCompany?.contains(tag) == true
+                        rankViewModel.rankParam.fundCompany?.contains(tag) == true
                 ) {
                     isChecked = true
                 }
             }
         }
-        mSortGroup?.forEach {
+        binding?.mSortGroup?.forEach {
             (it as? Chip)?.apply {
                 if (tag != null &&
-                    rankViewModel.rankParam.sort == tag
+                        rankViewModel.rankParam.sort == tag
                 ) {
                     isChecked = true
                 }
             }
         }
-        useDesc.isChecked = rankViewModel.rankParam.asc != 0
-        dataSize.setText(rankViewModel.rankParam.pageSize.toString())
+        binding?.useDesc?.isChecked = rankViewModel.rankParam.asc != 0
+        binding?.dataSize?.setText(rankViewModel.rankParam.pageSize.toString())
     }
 
     private fun submitData() {
         val typeList = mutableListOf<String>()
-        mTypeGroup.checkedChipIds.forEach {
+        binding?.mTypeGroup?.checkedChipIds?.forEach {
             (view?.findViewById<Chip>(it)?.tag as? String)?.let { type ->
                 typeList.add(type)
             }
         }
         val companyList = mutableListOf<String>()
-        mCompanyGroup.checkedChipIds.forEach {
+        binding?.mCompanyGroup?.checkedChipIds?.forEach {
             (view?.findViewById<Chip>(it)?.tag as? String)?.let { comp ->
                 companyList.add(comp)
             }
         }
         rankViewModel.rankParam.fundType = typeList
         rankViewModel.rankParam.fundCompany = companyList
-        rankViewModel.rankParam.sort = view?.findViewById<Chip>(
-                mSortGroup.checkedChipId
-        )?.tag as? String
-        rankViewModel.rankParam.asc = if (useDesc.isChecked) 1 else 0
-        rankViewModel.rankParam.pageSize = dataSize.text.toString().toInt()
+        rankViewModel.rankParam.sort =
+                binding?.mSortGroup?.checkedChipId?.let {
+                    view?.findViewById<Chip>(
+                            it
+                    )?.tag
+                } as? String
+        rankViewModel.rankParam.asc = if (binding?.useDesc?.isChecked == true) 1 else 0
+        rankViewModel.rankParam.pageSize = binding?.dataSize?.text.toString().toInt()
         rankViewModel.fetchRankData()
     }
 
