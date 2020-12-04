e113c11b9a1faaa1ad2a66e07c409cc429c6c8ad
HuixingWang <whx24@live.com>
2020-12-04 12:52:44 +0800

    add fragment viewBinding util

--- app/src/main/java/com/huixing/financial/utils/FragmentExt.kt
+++ app/src/main/java/com/huixing/financial/utils/FragmentExt.kt
@@ -1,11 +1,13 @@
 package com.huixing.financial.utils
 
+import android.view.View
 import androidx.annotation.MainThread
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelLazy
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
+import androidx.viewbinding.ViewBinding
 import com.huixing.financial.base.App
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 @MainThread
 inline fun <reified VM : ViewModel> Fragment.appViewModels(
@@ -16,3 +18,42 @@ inline fun <reified VM : ViewModel> Fragment.appViewModels(
     }
     return ViewModelLazy(VM::class, {(requireActivity().applicationContext as App).viewModelStore }, factoryPromise)
 }
+
+
+class FragmentViewBindingDelegate<T : ViewBinding>(
+    val fragment: Fragment,
+    val viewBindingFactory: (View) -> T
+) : ReadOnlyProperty<Fragment, T> {
+    private var binding: T? = null
+
+    init {
+        fragment.lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onCreate(owner: LifecycleOwner) {
+                fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
+                    viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+                        override fun onDestroy(owner: LifecycleOwner) {
+                            binding = null
+                        }
+                    })
+                }
+            }
+        })
+    }
+
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+        val binding = binding
+        if (binding != null) {
+            return binding
+        }
+
+        val lifecycle = fragment.viewLifecycleOwner.lifecycle
+        if (!lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
+            throw IllegalStateException("Should not attempt to get bindings when Fragment views are destroyed.")
+        }
+
+        return viewBindingFactory(thisRef.requireView()).also { this.binding = it }
+    }
+}
+
+fun <T : ViewBinding> Fragment.viewBinding(viewBindingFactory: (View) -> T) =
+    FragmentViewBindingDelegate(this, viewBindingFactory)
