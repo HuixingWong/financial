6088770854451d8b8e396cd6f691460169a5746b
HuixingWang <whx24@live.com>
2020-12-26 12:28:09 +0800

    remove token

--- app/src/main/java/com/huixing/financial/network/HttpRequestInterceptor.kt
+++ app/src/main/java/com/huixing/financial/network/HttpRequestInterceptor.kt
@@ -1,15 +1,14 @@
 package com.huixing.financial.network
 
-import com.huixing.financial.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 import timber.log.Timber
 
 class HttpRequestInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val originalRequest = chain.request()
-        val request = originalRequest.newBuilder().url(originalRequest.url)
-            .header("token",BuildConfig.ACCESS_TOKEN).build()
+        val request = chain.request()
+//        val request = originalRequest.newBuilder().url(originalRequest.url)
+//            .header("token",BuildConfig.ACCESS_TOKEN).build()
         Timber.d(request.toString())
         return chain.proceed(request)
     }
