331b0633911ce6128e21d158fe8ab84e2d07b814
HuixingWang <whx24@live.com>
2020-12-30 12:53:57 +0800

    use @Parcelize

--- app/src/main/java/com/huixing/financial/model/BaseDetail.kt
+++ app/src/main/java/com/huixing/financial/model/BaseDetail.kt
@@ -1,23 +1,18 @@
 package com.huixing.financial.model
 
-import android.os.Parcel
-import android.os.Parcelable
 import com.squareup.moshi.JsonClass
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
 
+@Parcelize
 @JsonClass(generateAdapter = true)
 data class BaseDetail(
-    val code: Int?,
-    val `data`: List<Data>?,
-    val message: String?,
-    val meta: String?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-            parcel.readValue(Int::class.java.classLoader) as? Int,
-            parcel.createTypedArrayList(Data),
-            parcel.readString(),
-            parcel.readString()) {
-    }
-
+        val code: Int?,
+        val `data`: @RawValue List<Data>?,
+        val message: String?,
+        val meta: String?
+) {
+    @Parcelize
     @JsonClass(generateAdapter = true)
     data class Data(
             val code: String?,
@@ -33,73 +28,5 @@ data class BaseDetail(
             val name: String?,
             val netWorth: Double?,
             val netWorthDate: String?
-    ) : Parcelable {
-        constructor(parcel: Parcel) : this(
-                parcel.readString(),
-                parcel.readString(),
-                parcel.readString(),
-                parcel.readValue(Double::class.java.classLoader) as? Double,
-                parcel.readString(),
-                parcel.readString(),
-                parcel.readString(),
-                parcel.readString(),
-                parcel.readString(),
-                parcel.readString(),
-                parcel.readString(),
-                parcel.readValue(Double::class.java.classLoader) as? Double,
-                parcel.readString()) {
-        }
-
-        override fun writeToParcel(parcel: Parcel, flags: Int) {
-            parcel.writeString(code)
-            parcel.writeString(dayGrowth)
-            parcel.writeString(expectGrowth)
-            parcel.writeValue(expectWorth)
-            parcel.writeString(expectWorthDate)
-            parcel.writeString(lastMonthGrowth)
-            parcel.writeString(lastSixMonthsGrowth)
-            parcel.writeString(lastThreeMonthsGrowth)
-            parcel.writeString(lastWeekGrowth)
-            parcel.writeString(lastYearGrowth)
-            parcel.writeString(name)
-            parcel.writeValue(netWorth)
-            parcel.writeString(netWorthDate)
-        }
-
-        override fun describeContents(): Int {
-            return 0
-        }
-
-        companion object CREATOR : Parcelable.Creator<Data> {
-            override fun createFromParcel(parcel: Parcel): Data {
-                return Data(parcel)
-            }
-
-            override fun newArray(size: Int): Array<Data?> {
-                return arrayOfNulls(size)
-            }
-        }
-
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeValue(code)
-        parcel.writeTypedList(data)
-        parcel.writeString(message)
-        parcel.writeString(meta)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<BaseDetail> {
-        override fun createFromParcel(parcel: Parcel): BaseDetail {
-            return BaseDetail(parcel)
-        }
-
-        override fun newArray(size: Int): Array<BaseDetail?> {
-            return arrayOfNulls(size)
-        }
-    }
+    )
 }
